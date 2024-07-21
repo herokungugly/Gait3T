a635c3234068133f17f346d4176d8d6b56934270
herokungugly <63494395+herokungugly@users.noreply.github.com>
2024-07-21 17:31:51 +0800

    Update custom_bce.py
    cahnged cardinality to not use [1,-1] mask

--- opengait/modeling/losses/custom_bce.py
+++ opengait/modeling/losses/custom_bce.py
@@ -11,14 +11,14 @@ class ClipBinaryCrossEntropyLoss(BaseLoss):
     def forward(self, projections, targets):
 
         device = torch.device("cuda") if projections.is_cuda else torch.device("cpu")
-        mask_similar_class = (targets.unsqueeze(1).repeat(1, targets.shape[0]) == targets).to(device)
-        mask_similar_class = mask_similar_class.int() * 2 - 1
+        mask_similar_class_bool = (targets.unsqueeze(1).repeat(1, targets.shape[0]) == targets).to(device)
+        mask_similar_class = mask_similar_class_bool.int() * 2 - 1
 
         dot_product_tempered = projections / self.temperature - self.B
         sigmoid_dot_product = torch.sigmoid(dot_product_tempered*mask_similar_class)
         # sigmoid_dot_product = torch.sigmoid(dot_product_tempered)
         
-        cardinality_per_samples = torch.sum(mask_similar_class, dim = 1)
+        cardinality_per_samples = torch.sum(mask_similar_class_bool, dim = 1)
 
         log_prob = torch.log(sigmoid_dot_product)
         # binary_crossentropy_loss_per_sample = torch.sum(log_prob * mask_similar_class, dim=1, keepdim=True) / cardinality_per_samples
