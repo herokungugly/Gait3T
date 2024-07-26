9a2e6952547092e7a9f053288ef746363408a8e1
herokungugly <63494395+herokungugly@users.noreply.github.com>
2024-07-26 09:13:15 +0800

    Update custom_bce.py
    added learnable param args

--- opengait/modeling/losses/custom_bce.py
+++ opengait/modeling/losses/custom_bce.py
@@ -8,23 +8,19 @@ class ClipBinaryCrossEntropyLoss(BaseLoss):
         self.B = B
         self.temperature = temperature
 
-    def forward(self, projections, targets):
+    def forward(self, projections, targets, B, temperature):
 
         device = torch.device("cuda") if projections.is_cuda else torch.device("cpu")
         mask_similar_class_bool = (targets.unsqueeze(1).repeat(1, targets.shape[0]) == targets).to(device)
         mask_similar_class = mask_similar_class_bool.int() * 2 - 1
         
-        dot_product_tempered = projections / self.temperature - self.B
+        dot_product_tempered = projections / temperature - B
         sigmoid_dot_product = torch.sigmoid(dot_product_tempered*mask_similar_class)
-        # sigmoid_dot_product = torch.sigmoid(dot_product_tempered)
         
         cardinality_per_samples = torch.sum(mask_similar_class_bool, dim = 1)
 
         log_prob = -torch.log(sigmoid_dot_product)
-        # print(log_prob.shape)
         log_prob = torch.sum(log_prob, dim = 1)
-        # print(log_prob.shape)
-        # binary_crossentropy_loss_per_sample = torch.sum(log_prob * mask_similar_class, dim=1, keepdim=True) / cardinality_per_samples
         binary_crossentropy_loss_per_sample = torch.sum(log_prob, dim=1, keepdim=True) / (cardinality_per_samples*targets.shape[0])
         binary_crossentropy_loss = torch.mean(binary_crossentropy_loss_per_sample)
 
